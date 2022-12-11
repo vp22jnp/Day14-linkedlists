45b836e72d8650e85f1de0f1fac158fc0c55b5fb
vp22j <vp22jnp@gmail.com>
2022-12-11 21:55:46 +0530

    added : welcome message

---
+++ src/main/java/com/day14/WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.day14;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("welcome to LinkedList dataStructures program ");
+    }
+}
